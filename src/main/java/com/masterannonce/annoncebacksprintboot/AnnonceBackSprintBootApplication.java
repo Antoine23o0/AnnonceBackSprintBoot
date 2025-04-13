54359c756cf148c73b83e69b6aa233485ac4b624
Antoine23o0 <ngatchouantoin1@gmail.com>
2025-04-13 12:39:24 +0200

    Initial commit du projet existant

---
+++ src/main/java/com/masterannonce/annoncebacksprintboot/AnnonceBackSprintBootApplication.java
@@ -0,0 +1,13 @@
+package com.masterannonce.annoncebacksprintboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnonceBackSprintBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnonceBackSprintBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/masterannonce/annoncebacksprintboot/AnnonceBackSprintBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masterannonce.annoncebacksprintboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnonceBackSprintBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
